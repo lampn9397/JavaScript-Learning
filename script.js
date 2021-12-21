3f326ebaa57d83f695d7d863464a0e5ec5b7d0c9
duongrong2012 <67502432+duongrong2012@users.noreply.github.com>
2021-12-21 20:10:23 +0700

    capnhat

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 //tri
+function buttonClick(buttonId) {
+    const x = document.getElementById('ButtonId');
+    document.getElementById("result").value = x.value
+}
+
+function onClick() {
+
+    // const x = document.getElementById('seven');
+    // alert(x.value)
+
+    // document.getElementById("result").value = x.value
+    let one = document.querySelector("#seven");
+    // one.addEventListener('click', function () { buttonClick(seven) })
+    one.addEventListener('click', function () {
+        // const x = document.getElementById('seven');
+        // document.getElementById("result").value = x.value
+        let input = document.getElementsByClassName("group-button").value
+        input[0].value
+    })
+
+    let two = document.querySelector("#eight");
+    // one.addEventListener('click', function () { buttonClick(seven) })
+    two.addEventListener('click', function () {
+        const x = document.getElementById('eight');
+        document.getElementById("result").value = x.value
+    })
+
+}
+
