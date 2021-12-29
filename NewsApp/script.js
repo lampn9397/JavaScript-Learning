440da6678a3390dfb112a6323a2701087f0009ed
Phan Ngoc Lam <lampn9397@gmail.com>
2021-12-29 21:30:31 +0700

    Update UI

--- NewsApp/script.js
+++ NewsApp/script.js
@@ -2,33 +2,53 @@
 const sampleText = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
 
 const news = [
-  { id: 1, title: 'Bai viet 1', body: sampleText, author: 'Tri Ngo' },
-  { id: 2, title: 'Bai viet 2', body: sampleText, author: 'Tri Ngo' },
-  { id: 3, title: 'Bai viet 3', body: sampleText, author: 'Tri Ngo' },
+  { id: 1, title: 'Bai viet 1', body: sampleText, author: 'Tri Ngo', image: 'https://www.si.com/.image/t_share/MTc3NTc2OTI0NDgxMDcwNzAx/top100_dchorizontal.jpg' },
+  { id: 2, title: 'Bai viet 2', body: sampleText, author: 'Tri Ngo', image: 'https://www.si.com/.image/t_share/MTc3NTc2OTI0NDgxMDcwNzAx/top100_dchorizontal.jpg' },
+  { id: 3, title: 'Bai viet 3', body: sampleText, author: 'Tri Ngo', image: 'https://www.si.com/.image/t_share/MTc3NTc2OTI0NDgxMDcwNzAx/top100_dchorizontal.jpg' },
 ];
 
 // Get thẻ div container để chèn item vào trong container
 const container = document.getElementsByClassName('container')[0];
 
-for (const item of news) {
+const createPostHTML = (item) => {
   // Tạo thẻ div item
   const divElement = document.createElement('div');
 
-  divElement.style.marginBottom = '8px';
+  // divElement.style.marginBottom = '8px';
+  divElement.className = 'item-container';
+
+  divElement.id = `post_${item.id}`;
 
   // Chèn dữ liệu vào thẻ div item
   divElement.innerHTML = `
-    ${item.id} - ${item.title} - ${item.author}
-    <button id="${item.id}" class="delete-button">
-      Delete
-    </button>
-    <button id="${item.id}" class="edit-button" author="${item.author}">
-      Edit
-    </button>
+    <img src="${item.image}" class="item-image" />
+
+    <div class="item-content">
+      <div class="item-title">${item.title}</div>
+
+      <div class="item-title-separator">
+        <div class="item-title-separator-inner"></div>
+      </div>
+
+      <div class="item-body">${item.body}</div>
+
+      <div>
+        <button id="${item.id}" class="delete-button">
+          Delete
+        </button>
+        <button id="${item.id}" class="edit-button" author="${item.author}">
+          Edit
+        </button>
+      </div>
+    </div>
   `;
 
   // Thêm thẻ div item vào thẻ div container
   container.appendChild(divElement);
+}
+
+for (const item of news) {
+  createPostHTML(item);
 }
 
 const deleteButtons = document.getElementsByClassName('delete-button');
@@ -40,7 +60,9 @@ const onClickDelete = function () {
 
   if (!isConfirmed) return;
 
-  this.parentElement.remove();
+  const postElement = document.querySelector(`div#post_${this.id}.item-container`);
+
+  postElement.remove();
 }
 
 for (const button of deleteButtons) {
