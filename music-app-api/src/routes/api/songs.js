6432f3abfbf53429266b4c34d7b3f547b7ea9082
Phan Ngoc Lam <lampn9397@gmail.com>
2022-01-24 21:42:57 +0700

    Update hot music for slide

--- music-app-api/src/routes/api/songs.js
+++ music-app-api/src/routes/api/songs.js
@@ -36,7 +36,7 @@ router.get('/list', async (req, res, next) => {
 
 router.get('/rank', async (req, res, next) => {
   try {
-    let songLocations = await SongLocation.aggregate([
+    const songLocations = await SongLocation.aggregate([
       {
         $lookup: {
           from: 'songs',
@@ -44,7 +44,11 @@ router.get('/rank', async (req, res, next) => {
           foreignField: 'songs_locations',
           as: 'songs',
           pipeline: [{
-            $sort: { views: -1 }
+            $sort: { views: -1 },
+            $project: {
+              categories: 0,
+              songs_locations: 0,
+            }
           }]
         }
       }
@@ -53,6 +57,32 @@ router.get('/rank', async (req, res, next) => {
     res.json(createResponse({
       ok: true,
       results: songLocations
+    }));
+  } catch (error) {
+    res.status(500).json(createResponse({
+      ok: false,
+      message: 'Failed to get music by country',
+      error: error.message
+    }));
+  }
+});
+
+router.get('/hot', async (req, res, next) => {
+  try {
+    const songs = await Song.aggregate([
+      { $sort: { views: -1 } },
+      { $limit: 6 },
+      {
+        $project: {
+          categories: 0,
+          songs_locations: 0,
+        }
+      }
+    ]);
+
+    res.json(createResponse({
+      ok: true,
+      results: songs
     }));
   } catch (error) {
     res.status(500).json(createResponse({
