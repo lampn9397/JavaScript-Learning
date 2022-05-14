4498ca693c493a112e98be936c8834cc7f5aa834
Phan Ngoc Lam <lampn9397@gmail.com>
2022-05-14 23:49:40 +0700

    Remove console.log

--- messenger-clone-api/src/routes/api/user/middlewares.js
+++ messenger-clone-api/src/routes/api/user/middlewares.js
@@ -3,14 +3,9 @@ import multer from 'multer';
 const storage = multer.diskStorage({
   destination: 'public/images/user_avatar/',
   filename: (req, file, cb) => {
-    console.log('filename > ', filename);
-
     const fileExtensionArray = file.originalname.split('.');
 
     const fileExtension = fileExtensionArray[fileExtensionArray.length - 1];
-
-    console.log('req.user._id > ', req.user._id);
-    console.log('fileExtension > ', fileExtension);
 
     cb(null, `${req.user._id}.${fileExtension}`);
   },
@@ -23,8 +18,6 @@ export const userAvatarMulter = multer({
     fileSize: 5242880, // 5 MB
   },
   fileFilter: function (req, file, cb) {
-    console.log('file > ', file);
-
     const isImage = file.mimetype.startsWith('image/');
 
     if (!isImage) {
