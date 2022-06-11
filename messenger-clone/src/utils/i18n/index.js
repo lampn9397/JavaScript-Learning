72cc3e6a88f8f876108a462ea101b243bd14473d
Phan Ngoc Lam <lampn9397@gmail.com>
2022-06-11 23:24:03 +0700

    Optimize i18n

--- messenger-clone/src/utils/i18n/index.js
+++ messenger-clone/src/utils/i18n/index.js
@@ -9,20 +9,25 @@ export const languageOption = {
     vi,
 };
 
+let resources = Object.keys(languageOption).map((key) => [key, { translation: languageOption[key] }]);
+
+resources = Object.fromEntries(resources);
+
 i18n
     .use(initReactI18next) // passes i18n down to react-i18next
     .init({
         // the translations
         // (tip move them in a JSON file and import them,
         // or even better, manage them via a UI: https://react.i18next.com/guides/multiple-translation-files#manage-your-translations-with-a-management-gui)
-        resources: {
-            en: {
-                translation: en
-            },
-            vi: {
-                translation: vi
-            }
-        },
+        // resources: {
+        //     en: {
+        //         translation: en
+        //     },
+        //     vi: {
+        //         translation: vi
+        //     }
+        // },
+        resources,
         lng: "vi", // if you're using a language detector, do not define the lng option
         fallbackLng: "en",
 
