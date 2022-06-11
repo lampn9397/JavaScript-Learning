d5ecaf0e69e9b8647362792132426146f6164749
Phan Ngoc Lam <lampn9397@gmail.com>
2022-06-11 20:19:32 +0700

    Update authorization for socket

--- messenger-clone-api/src/services/socket.js
+++ messenger-clone-api/src/services/socket.js
@@ -1,4 +1,7 @@
 import { Server } from 'socket.io';
+import jwt from 'jsonwebtoken';
+import { jwtOptions } from '../app';
+import User from '../models/User';
 
 export const SocketEvents = {
   NEW_MESSAGE: 'new-message',
@@ -9,7 +12,27 @@ export const createSocketServer = (server) => {
   const io = new Server(server, {
     cors: {
       origin: '*',
+    },
+  });
+
+  io.use(async (socket, next) => {
+    const { token } = socket.handshake.auth;
+
+    if (!token) return next(new Error('Invalid token.'));
+
+    const { JWT_SECRET } = process.env;
+
+    const payload = jwt.verify(token, JWT_SECRET);
+
+    try {
+      const user = await User.findById(payload.id).lean();
+
+      if (!user) return next(new Error('Invalid token.'));
+    } catch (error) {
+      return next(error);
     }
+
+    next();
   });
 
   io.on("connection", (socket) => {
