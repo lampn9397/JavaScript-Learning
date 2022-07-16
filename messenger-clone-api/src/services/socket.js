e8b4e56f2ee1c01b80acb908bf556c683b550c35
Phan Ngoc Lam <lampn9397@gmail.com>
2022-07-16 22:11:52 +0700

    Update socket for user online

--- messenger-clone-api/src/services/socket.js
+++ messenger-clone-api/src/services/socket.js
@@ -2,6 +2,8 @@ import { Server } from 'socket.io';
 import jwt from 'jsonwebtoken';
 
 import User from '../models/User';
+import Conversation from '../models/Conversation';
+import { getConversationTitle } from '../routes/api/chat/middlewares';
 
 export const SocketEvents = {
   NEW_MESSAGE: 'new-message',
@@ -45,17 +47,36 @@ export const createSocketServer = (server) => {
 
     socket.join(`user_${result.id}`);
 
-    await User.updateOne({ _id: result.id }, {
-      online: true,
-      lastLogin: new Date(),
-    });
-
     socket.on('disconnect', async () => {
       await User.updateOne({ _id: result.id }, {
         online: false,
         lastLogin: new Date(),
       });
     });
+
+    await User.updateOne({ _id: result.id }, {
+      online: true,
+      lastLogin: new Date(),
+    });
+
+    const conversations = await Conversation.find({ users: result.id })
+      .populate('users', 'firstName lastName avatar online lastLogin')
+      .populate({
+        path: 'lastMessage',
+        populate: {
+          path: 'user',
+          select: 'firstName lastName avatar'
+        }
+      })
+      .lean({ getters: true });
+
+    conversations.forEach((c, index) => {
+      const title = getConversationTitle(item, req);
+
+      c.users.filter((x) => x._id !== result.id).forEach((u) => {
+        io.in(`user_${u._id}`).emit(SocketEvents.NEW_CONVERSATION, { ...c, title });
+      });
+    })
   });
 
   return io;
