137a575cb671dc49fc47a61accc0e3bd99f37e63
duongrong2012 <67502432+duongrong2012@users.noreply.github.com>
2023-01-14 11:14:40 +0700

    update

---
+++ story-app/src/components/Carousel/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import styles from './style.module.css'
+
+function Carousel({ }) {
+
+    return (
+        <div className={`${styles.CarouselContainer}`}>
+        </div>
+    )
+}
+
+Carousel.propTypes = {
+}
+
+Carousel.defaultProps = {
+}
+
+export default Carousel;
